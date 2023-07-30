47985fd99e545501ee1a95f23c7d6e92310f4463
zahid <syed.zahid.ali.sha@gmail.com>
2023-07-30 18:55:07 +0500

    Simple pytest examples

---
+++ pytest_learning/test_my_logic.py
@@ -0,0 +1,14 @@
+def test__my_first_test__passed():
+    assert 10 == 10
+
+
+def test__my_second_test__failed():
+    assert 10 == 11
+
+
+def test__my_test__passed():
+    assert 100 == 100
+
+
+def test__my_test__failed():
+    assert 100 == 101
